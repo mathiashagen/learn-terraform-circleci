922ab77e4e6db19f6ca4cef9f0cccf04eeacb6ee
Mathias Hagen <mathhag@stud.ntnu.no>
2021-12-06 15:03:37 +0100

    test

--- main.tf
+++ main.tf
@@ -75,3 +75,32 @@ resource "aws_s3_bucket_object" "app" {
 output "Endpoint" {
   value = aws_s3_bucket.app.website_endpoint
 }
+
+resource "aws_s3_bucket" "app" {
+  tags = {
+    Name = "App Bucket"
+  }
+
+  bucket = "${var.app}.${var.label}.${random_uuid.randomid.result}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  force_destroy = true
+
+}
+
+resource "aws_s3_bucket_object" "app" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.app.id
+  content      = file("./assets/index.html")
+  content_type = "text/html"
+
+}
+
+output "Endpoint" {
+  value = aws_s3_bucket.app.website_endpoint
+}
